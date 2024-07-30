468a7e102bc5558dd892a561f361873eb3319e89
Bruno Oliveira <brunno.oliver7@gmail.com>
2024-07-29 21:43:01 -0300

    70 - Climbing Stairs

---
+++ leetcode/70 - Climbing Stairs/solution2.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+  if (n <= 3) return n;
+
+  let arr = [1, 1];
+
+  for (let i = 2; i <= n; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+
+  return arr[n];
+};
