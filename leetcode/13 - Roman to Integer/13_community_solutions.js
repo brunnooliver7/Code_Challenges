29602ed5e71f7aa841dd296f85edf21cded38b6e
Bruno Oliveira <brunno.oliver7@gmail.com>
2024-07-31 18:49:33 -0300

    13 - Roman to Integer

--- leetcode/13 - Roman to Integer/13_community_solutions.js
+++ leetcode/13 - Roman to Integer/13_community_solutions.js
@@ -1,17 +1,27 @@
-symbols = {
-  "I": 1,
-  "V": 5,
-  "X": 10,
-  "L": 50,
-  "C": 100,
-  "D": 500,
-  "M": 1000
-};
-
 var romanToInt = function (s) {
-  value = 0;
-  for (let i = 0; i < s.length; i += 1) {
-      symbols[s[i]] < symbols[s[i + 1]] ? value -= symbols[s[i]] : value += symbols[s[i]]
+  const sym = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let result = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const cur = sym[s[i]];
+    const next = sym[s[i + 1]];
+
+    if (cur < next) {
+      result += next - cur;
+      i++;
+    } else {
+      result += cur;
+    }
   }
-  return value
+
+  return result;
 };
--- leetcode/13 - Roman to Integer/13_solution.js
+++ leetcode/13 - Roman to Integer/13_solution.js
@@ -23,47 +23,15 @@ var romanToInt = function (s) {
   special.set("CM", 900);
 
   let res = 0;
-  let skip = false;
 
-  primeiroFor: for (let i = 0; i < s.length; i++) {
-    if (skip) {
-      skip = false;
+  for (let i = 0; i < s.length; i++) {
+    if (special.has(s.charAt(i) + s.charAt(i + 1))) {
+      res += special.get(s.charAt(i) + s.charAt(i + 1));
+      i++;
       continue;
     }
 
-    for (var key of special.keys()) {
-      if (key === s.charAt(i) + s.charAt(i + 1)) {
-        res += special.get(s.charAt(i) + s.charAt(i + 1));
-        skip = true;
-        continue primeiroFor;
-      }
-    }
-
     res += base.get(s.charAt(i));
-  }
-
-  return res;
-};
-
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function (s) {
-
-  let symbols = new Map();
-  symbols.set("I", 1);
-  symbols.set("V", 5);
-  symbols.set("X", 10);
-  symbols.set("L", 50);
-  symbols.set("C", 100);
-  symbols.set("D", 500);
-  symbols.set("M", 1000);
-
-  let res = 0;
-
-  for (let i = 0; i < s.length; i++) {
-      symbols.get(s[i]) < symbols.get(s[i + 1]) ? res -= symbols.get(s[i]) : res += symbols.get(s[i])
   }
 
   return res;
