f8a70b2405f282aade8671dbea7165c9cbb7d1bf
Bruno Oliveira <brunno.oliver7@gmail.com>
2024-08-14 20:03:24 -0300

    1 - Two Sum

--- leetcode/1 - Two Sum/Solution.js
+++ leetcode/1 - Two Sum/Solution.js
@@ -1,9 +1,10 @@
+// Brutal Force
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
+var twoSumBrutal = function (nums, target) {
   for (let i = 0; i < nums.length - 1; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       let n1 = nums[i];
@@ -14,3 +15,27 @@ var twoSum = function (nums, target) {
     }
   }
 };
+
+// Hash Table
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function twoSumHashMap(nums, target) {
+  let m = new Map()
+
+  for (let i = 0; i < nums.length; i++) {
+    const wantedValue = target - nums[i]
+    if (m.has(wantedValue)) return [m.get(wantedValue), i]
+    m.set(nums[i], i)
+  }
+
+  return null
+}
+
+const t1 = twoSumHashMap([1, 3, 7, 9, 2], 11)
+console.log(t1)
+
+const t2 = twoSumHashMap([], 11)
+console.log(t2)
